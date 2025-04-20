5517be1234e5277f84768724f7e144c4c3d0f82f
Mirko Sangrigoli <mirko.sangrigoli@gmail.com>
2025-04-20 17:48:47 +0100

    unify filesystem and hierarchical view

--- MethodTrackerVisualizer/Views/HierarchicalView.xaml.cs
+++ MethodTrackerVisualizer/Views/HierarchicalView.xaml.cs
@@ -2,85 +2,101 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Threading;
 using MethodTrackerVisualizer.Helpers;
 
-namespace MethodTrackerVisualizer.Views;
-
-public partial class HierarchicalView
+namespace MethodTrackerVisualizer.Views
 {
-    private List<LogEntry> _matchedTextEntries = [];
-    private int _currentMatchTextEntriesIndex = -1;
-
-    public string CurrentSearchText
+    public partial class HierarchicalView : UserControl
     {
-        get => (string)GetValue(CurrentSearchTextProperty);
-        set => SetValue(CurrentSearchTextProperty, value);
-    }
+        private List<LogEntry> _matchedTextEntries = new();
+        private int _currentMatchTextEntriesIndex = -1;
 
-    public static readonly DependencyProperty CurrentSearchTextProperty =
-        DependencyProperty.Register(nameof(CurrentSearchText), typeof(string), typeof(HierarchicalView), new PropertyMetadata(string.Empty));
-
-    public HierarchicalView()
-    {
-        InitializeComponent();
-        Loaded += Load;
-    }
-
-    public void Load(object sender, RoutedEventArgs e)
-    {
-        HierarchicalTreeView.ItemsSource = FileHelper.Selected?.Data ?? [];
-        HierarchicalSearchBar.SearchTextChanged += SearchForText;
-        HierarchicalSearchBar.PreviousClicked += PreviousText;
-        HierarchicalSearchBar.NextClicked += NextText;
-    }
-
-    private void SearchForText(object _, string searchText)
-    {
-        CurrentSearchText = searchText.Trim();
-        if (string.IsNullOrEmpty(CurrentSearchText))
+        public HierarchicalView()
         {
+            InitializeComponent();
+            Loaded += OnLoaded;
+            // Update tree whenever the selected file changes
+            FileHelper.Refresh += (_, __) => Dispatcher.Invoke(RefreshTree);
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            // Initial population of the TreeView
+            RefreshTree();
+
+            // Hook up search navigation
+            HierarchicalSearchBar.SearchTextChanged += SearchForText;
+            HierarchicalSearchBar.PreviousClicked += PreviousText;
+            HierarchicalSearchBar.NextClicked += NextText;
+        }
+
+        private void RefreshTree()
+        {
+            HierarchicalTreeView.ItemsSource =
+                FileHelper.Selected?.Data ?? new List<LogEntry>();
+
             _matchedTextEntries.Clear();
             _currentMatchTextEntriesIndex = -1;
-            return;
         }
 
-        _matchedTextEntries = FileHelper.Selected?.Data.FindMatchingText(CurrentSearchText) ?? [];
-        if (_matchedTextEntries.Any())
+        private void SearchForText(object _, string searchText)
         {
-            _currentMatchTextEntriesIndex = 0;
+            var trimmed = searchText.Trim();
+            if (string.IsNullOrEmpty(trimmed))
+            {
+                _matchedTextEntries.Clear();
+                _currentMatchTextEntriesIndex = -1;
+                return;
+            }
+
+            var data = FileHelper.Selected?.Data;
+            if (data == null || data.Count == 0)
+                return;
+
+            // Fast background search
+            _matchedTextEntries = data
+                .FindMatchingText(trimmed)
+                .ToList();
+
+            if (_matchedTextEntries.Any())
+            {
+                _currentMatchTextEntriesIndex = 0;
+                NavigateToEntry(_matchedTextEntries[0]);
+            }
+        }
+
+        private void PreviousText(object sender, EventArgs e)
+        {
+            if (!_matchedTextEntries.Any()) return;
+
+            _currentMatchTextEntriesIndex =
+                (_currentMatchTextEntriesIndex - 1 + _matchedTextEntries.Count)
+                % _matchedTextEntries.Count;
+
             NavigateToEntry(_matchedTextEntries[_currentMatchTextEntriesIndex]);
         }
-    }
 
-    private void PreviousText(object sender, EventArgs _)
-    {
-        if (_matchedTextEntries.Any())
+        private void NextText(object sender, EventArgs e)
         {
-            _currentMatchTextEntriesIndex = (_currentMatchTextEntriesIndex - 1 + _matchedTextEntries.Count) % _matchedTextEntries.Count;
+            if (!_matchedTextEntries.Any()) return;
+
+            _currentMatchTextEntriesIndex =
+                (_currentMatchTextEntriesIndex + 1) % _matchedTextEntries.Count;
+
             NavigateToEntry(_matchedTextEntries[_currentMatchTextEntriesIndex]);
         }
-    }
 
-    private void NextText(object sender, EventArgs _)
-    {
-        if (_matchedTextEntries.Any())
+        private void NavigateToEntry(LogEntry entry)
         {
-            _currentMatchTextEntriesIndex = (_currentMatchTextEntriesIndex + 1) % _matchedTextEntries.Count;
-            NavigateToEntry(_matchedTextEntries[_currentMatchTextEntriesIndex]);
+            HierarchicalTreeView.ExpandAllParents(entry);
+            Dispatcher.BeginInvoke(new Action(() =>
+            {
+                var tvi = HierarchicalTreeView.GetTreeViewItem(entry);
+                tvi?.ExpandExpanderForEntry();
+                tvi?.BringIntoView();
+            }), DispatcherPriority.Background);
         }
     }
-
-    private void NavigateToEntry(object dataItem)
-    {
-
-        HierarchicalTreeView.ExpandAllParents(dataItem);
-        Dispatcher.BeginInvoke(new Action(() =>
-        {
-            var tvi = HierarchicalTreeView.GetTreeViewItem(dataItem);
-            tvi.ExpandExpanderForEntry();
-            tvi.BringIntoView();
-        }), DispatcherPriority.Background);
-    }
-
 }
